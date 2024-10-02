3578978d515761785a80116a68e3474a0bc8dd43
JYap <justinyap8818@gmail.com>
2024-10-02 10:25:14 -0400

    dotenv remove

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { promises as fs } from 'fs';
 import dotenv from 'dotenv';
 import { MongoClient, ObjectId } from 'mongodb';
-import dotenv from 'dotenv';
 import cors from 'cors';
 
 dotenv.config();
